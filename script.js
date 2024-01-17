d452c61c3b73627c7693420befeef67ddd9c8053
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2024-01-17 22:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	if (text === "") {
+		return "";
+	}
+	else{
+		return text.charAt(0);
+	}
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+
